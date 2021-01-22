2067b2fdfaf18778fe6c801229e1cd51c59a8da3
Greyscale <envvirtupsdummy1@gmail.com>
2021-01-22 07:27:44 +0700

    second Revisi

---
+++ cypress/integration/Web/Login_Normal.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+let user
+
+describe('aUTH', () => {
+
+        it('Login Evermos Account', () => {
+            cy.visit('https://evermos.com/')
+            cy.get(':nth-child(6) > a').click('')
+
+            cy.get('[style="margin-bottom:30px;"] > .inputText__inner > .inputText__input').type('621223334444')
+            cy.get('[style="margin-bottom:15px;"] > .inputText__inner > .inputText__input').type('password')
+            cy.get('.btn').click('')
+            .then((res) => {
+              cy.log(res)
+
+              const OTP = res.body
+              const status = res.status
+              const duration = res.duration
+              const headers = res.headers
+              cy.writeFile('ccypress/fixtures/EvermoS_Test/Auth.json', { status: status, duration: duration, headers: headers, OTP })
+            })
+
+        })
+})
